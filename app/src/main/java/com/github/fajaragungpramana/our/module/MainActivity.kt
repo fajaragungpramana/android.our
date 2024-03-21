125e3d2de4694ec011b6b8bc871179a55aa05aec
fajaragungpramana <fajar.agungpramana01@gmail.com>
2024-03-21 11:40:12 +0700

    Add animation on splash and duration.

--- app/src/main/java/com/github/fajaragungpramana/our/module/MainActivity.kt
+++ app/src/main/java/com/github/fajaragungpramana/our/module/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.github.fajaragungpramana.our.module
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.animation.AnticipateInterpolator
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.github.fajaragungpramana.our.common.app.AppActivity
 import com.github.fajaragungpramana.our.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -8,10 +14,43 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppActivity<ActivityMainBinding>() {
 
-    override fun onViewBinding(): ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+    private var keep = true
+
+    override fun onViewBinding(): ActivityMainBinding {
+        installSplashScreen().let {
+            it.setKeepOnScreenCondition { keep }
+            it.setOnExitAnimationListener { viewProvider ->
+
+                val slideDown = ObjectAnimator.ofFloat(
+                    viewProvider.view,
+                    View.TRANSLATION_Y,
+                    0f,
+                    +viewProvider.view.height.toFloat()
+                )
+                slideDown.interpolator = AnticipateInterpolator()
+                slideDown.duration = 200L
+
+                slideDown.doOnEnd { viewProvider.remove() }
+
+                slideDown.start()
+            }
+        }
+
+        return ActivityMainBinding.inflate(layoutInflater)
+    }
 
     override fun onCreated(savedInstanceState: Bundle?) {
+        initView()
 
+        Handler(mainLooper).postDelayed({ keep = false }, DELAY)
+    }
+
+    private fun initView() {
+        setSupportActionBar(viewBinding.mtlMain)
+    }
+
+    private companion object {
+        const val DELAY = 3000L
     }
 
 }
